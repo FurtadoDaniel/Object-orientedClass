56e4776164a0e50afdc33ec030ecbd1adf8289a3
Daniel Furtado <Daniel.96.Furtado@gmail.com>
2017-03-31 01:24:56 -0300

    Add functions, but still not complet

---
+++ NameList.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class NameList {
+
+    public static void addName(String[] Array, String NewName) {
+
+        int len = (Array != null) ? Array.length : 0;
+
+        String[] NewArray = new String[len + 1];
+
+        for (int Counter = 0; Counter < len; Counter++) NewArray[Counter] = Array[Counter];
+        NewArray[len] = NewName;
+
+        Array = NewArray;
+    }
+
+    public static void loadList(String[] Names, String Archive) {
+        
+        String Line = null;
+        
+        try {
+
+            BufferedReader Lines = new BufferedReader(new FileReader(Archive));
+
+            while((Line = Lines.readLine()) != null) addName(Names, Line);
+
+            Lines.close();         
+        }
+
+        catch(FileNotFoundException ex){ System.out.println("Unable to open file '" + Archive + "'"); }
+        catch(IOException ex) {
+            System.out.println("Error reading file '"+ Archive + "'");
+        }
+    }
+
+    public static boolean verifyName(String Name) {
+        
+        Pattern p = Pattern.compile("[^a-z ]", Pattern.CASE_INSENSITIVE);
+        boolean HasInvalidCharacter = p.matcher(Name).find();
+        
+        boolean HasValidCharacter = false;
+        for (int Counter =0; Counter < Name.length(); Counter++) if (Name.charAt(Counter) != ' ') HasValidCharacter = true;
+
+        return (HasValidCharacter || HasInvalidCharacter);
+    }
+
+    public static String readName() {
+
+        Scanner Name_enter = new Scanner(System.in);
+        String Name;
+        do {
+            System.out.println("Informe o nome que deseja Adicionar:");
+            Name = Name_enter.nextLine();
+        } while (verifyName(Name));
+        return Name;
+    }
+
+
+    public static void printList(String[] List) {
+        if (List == null) return;
+        for (int Counter = 0 ; Counter < List.length; Counter++) System.out.println(List[Counter]);
+    }
+
+    public static void writeList(String[] List, String Archive){
+        try {
+            if (List == null) return;
+            
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(Archive));
+            for (int Counter = 0 ; Counter < List.length; Counter++) {
+                bufferedWriter.write(List[Counter]);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println("Error writing to file '" + Archive + "'");
+        }
+    }
+
+    public static boolean contains (int[] Array, int Value) {
+        for (int Counter = 0; Counter < Array.length; Counter++) if (Value == Array[Counter]) return true;
+        return false;
+    }
+
+    public static void main(String[] Args) {
+		String [] Names = null;
+		int Choose = -1;
+		final int[] Valid_options = {0,1,2,3,9};
+		
+		Scanner Option_enter = new Scanner(System.in);
+		do {
+			System.out.println("---NameList---\n\n0-Load\n1-Add a name\n2-Show\n3-Write\n9-EXIT");
+			do {
+				System.out.print("Choose:");
+				Choose = Option_enter.nextInt();
+			} while (! (contains(Valid_options, Choose)));
+			switch (Choose) {
+				case 0:loadList(Names, "people.txt");break;
+				case 1:addName(Names, readName());break;
+				case 2:printList(Names);break;
+				case 4:writeList(Names,"people.txt");break;
+				case 9:System.out.println("End");
+			}
+			System.out.print("\f");
+		} while (Choose != 9);
+	}
+	
+}
+
